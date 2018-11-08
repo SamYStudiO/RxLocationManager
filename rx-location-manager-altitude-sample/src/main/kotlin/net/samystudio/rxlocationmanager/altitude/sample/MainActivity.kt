de0ec2dbb96be8945d3134471ee38e04949e21ef
samystudio <contact@samystudio.net>
2018-11-08 21:32:30 +0100

    Add rx-location-manager-altitude-sample

---
+++ rx-location-manager-altitude-sample/src/main/kotlin/net/samystudio/rxlocationmanager/altitude/sample/MainActivity.kt
@@ -0,0 +1,73 @@
+package net.samystudio.rxlocationmanager.altitude.sample
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.tbruyelle.rxpermissions2.RxPermissions
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import net.samystudio.rxlocationmanager.altitude.GoogleElevationApi
+import net.samystudio.rxlocationmanager.altitude.RxLocationManagerAltitude
+
+class MainActivity : AppCompatActivity() {
+    private val compositeDisposable = CompositeDisposable()
+
+    @SuppressLint("MissingPermission")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val rxPermissions = RxPermissions(this)
+
+        compositeDisposable.add(
+            rxPermissions
+                .request(Manifest.permission.ACCESS_FINE_LOCATION)
+                .filter { it }
+                .flatMap { RxLocationManagerAltitude.observeGpsEllipsoidalAltitudeUpdates() }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { Log.d("GpsEllipsoidalAltitude", it.toString()) }
+        )
+
+        compositeDisposable.add(
+            rxPermissions
+                .request(Manifest.permission.ACCESS_FINE_LOCATION)
+                .filter { it }
+                .flatMap { RxLocationManagerAltitude.observeGpsGeoidalAltitudeUpdates() }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { Log.d("GpsGeoidalAltitude", it.toString()) }
+        )
+
+        compositeDisposable.add(
+            RxLocationManagerAltitude.observeBarometricAltitudeUpdates(
+                1000,
+                SensorManager.PRESSURE_STANDARD_ATMOSPHERE
+            )
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { Log.d("BarometricAltitude", it.toString()) }
+        )
+
+        compositeDisposable.add(
+            RxLocationManagerAltitude.getRemoteServiceAltitude(
+                GoogleElevationApi("your_google_api_key"),
+                48.866667,
+                2.333333
+            ).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    Log.d(
+                        "RemoteServiceAltitude",
+                        it.toString()
+                    )
+                }, { Log.d("RemoteServiceAltitude", "Error getting remote altitude") })
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        compositeDisposable.dispose()
+    }
+}
