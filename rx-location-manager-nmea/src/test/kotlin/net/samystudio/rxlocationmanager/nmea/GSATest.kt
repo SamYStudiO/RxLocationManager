bb1cb7832ce8329aa4f01b16f22cfc209b72d48f
Samuel Eminet <contact@samystudio.net>
2020-05-14 16:26:07 -0400

    Remove Robolectric for GSATest, don't need it here

--- rx-location-manager-nmea/src/test/kotlin/net/samystudio/rxlocationmanager/nmea/GSATest.kt
+++ rx-location-manager-nmea/src/test/kotlin/net/samystudio/rxlocationmanager/nmea/GSATest.kt
@@ -2,12 +2,7 @@ package net.samystudio.rxlocationmanager.nmea
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 
-@RunWith(RobolectricTestRunner::class)
-@Config(manifest = Config.NONE)
 class GSATest {
     @Test
     fun validate() {
