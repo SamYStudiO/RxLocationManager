e84847f8f435fef1898e1ba1c5fa65732339fa67
SamYStudiO <contact@samystudio.net>
2022-01-24 10:54:36 -0400

    Fix tests

--- rx-location-manager-nmea/src/test/kotlin/net/samystudio/rxlocationmanager/nmea/TimeValidatorTest.kt
+++ rx-location-manager-nmea/src/test/kotlin/net/samystudio/rxlocationmanager/nmea/TimeValidatorTest.kt
@@ -12,7 +12,7 @@ class TimeValidatorTest {
             assertTrue(it.validate("000000.00"))
             assertTrue(it.validate("000000.0000"))
             assertFalse(it.validate("0000000"))
-            assertFalse(it.validate("000000.0"))
+            assertTrue(it.validate("000000.0"))
             assertFalse(it.validate("000000.00000"))
             assertFalse(it.validate("A000000.00000"))
             assertFalse(it.validate("000000.00000A"))
