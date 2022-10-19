488fd62b1fa339340de58a3975c4721675c1eb3f
SamYStudiO <contact@samystudio.net>
2022-10-19 14:24:06 -0400

    Bump libraries

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,32 +1,32 @@
 object Versions {
-    const val compileSdk = 31
+    const val compileSdk = 32
     const val minSdk = 14
-    const val targetSdk = 31
+    const val targetSdk = 32
 
     // tools
-    const val android_build_tools = "7.2.1"
+    const val android_build_tools = "7.3.1"
     const val gradle_versions_plugin = "0.42.0"
-    const val gradle_maven_publish_plugin = "0.21.0"
+    const val gradle_maven_publish_plugin = "0.22.0"
     const val spotless = "6.8.0"
     const val ktlint = "0.46.0"
 
     // kotlin
-    const val kotlin = "1.7.10"
+    const val kotlin = "1.7.20"
 
     // android
-    const val core_ktx = "1.8.0"
+    const val core_ktx = "1.9.0"
     const val startup = "1.1.1"
-    const val appcompat = "1.4.2"
+    const val appcompat = "1.5.1"
 
     // reactive
     const val rxjava = "3.1.5"
     const val rxandroid = "3.0.0"
-    const val rxlocationmanager = "0.6.0"
+    const val rxlocationmanager = "0.7.0"
 
     //misc
     const val rxpermissions = "0.12"
 
     // test
     const val junit = "4.13.2"
-    const val mockk = "1.12.2"
+    const val mockk = "1.13.2"
 }
