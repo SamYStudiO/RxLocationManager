6db287ffb02da54093984a1c753474324e17aade
SamYStudiO <contact@samystudio.net>
2023-07-07 08:17:08 -0400

    Add compile options to sample as well

--- rx-location-manager-altitude-sample/build.gradle.kts
+++ rx-location-manager-altitude-sample/build.gradle.kts
@@ -29,6 +29,15 @@ android {
     buildFeatures {
         buildConfig = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
